16688d6d8da040a78e2409b14b75547de56f40f7
Ngisekona55 <137483529+Ngisekona55@users.noreply.github.com>
2024-10-18 06:42:31 +0300

    Add files via upload
    adding files

---
+++ numeric.py
@@ -0,0 +1,19 @@
+print("Python has three numeric types: int, float, and complex")
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myFloatValue = 3.14
+print(myFloatValue)
+print(str(myFloatValue) + " is of the data type " + str(type(myFloatValue)))
+
+myComplexValue = 5j
+print(myComplexValue)
+print(type(myComplexValue))
+print(str(myComplexValue) + " is of the data type " + str(type(myComplexValue)))
+
+myBoolDataValue = True
+print(myBoolDataValue)
+print(type(myBoolDataValue))
+print(str(myBoolDataValue) + " is of the data type " + str(type(myBoolDataValue)))
